9388f6362bf0c58f73f139f9b6f806a453bd721d
Mahesh <mahesh.moodley@thevirtualagent.co.za>
2024-03-15 15:53:58 +0200

    Complete FizzBuzzKata

--- FizzBuzzKata/FizzBuzzKata.Tests/TestFizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata.Tests/TestFizzBuzz.cs
@@ -15,6 +15,136 @@ public class TestFizzBuzz
         var result = fizzBuzz.GetResult(input);
         // Assert
         var expected = "1";
-        Assert.That(result,Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_2_ShouldReturn_String_2()
+    {
+        // Arrange
+        var input = 2;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "2";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_3_ShouldReturn_String_Fizz()
+    {
+        // Arrange
+        var input = 3;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "Fizz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_4_ShouldReturn_String_4()
+    {
+        // Arrange
+        var input = 4;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "4";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_5_ShouldReturn_String_Buzz()
+    {
+        // Arrange
+        var input = 5;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "Buzz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_6_ShouldReturn_String_Fizz()
+    {
+        // Arrange
+        var input = 6;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "Fizz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_9_ShouldReturn_String_Fizz()
+    {
+        // Arrange
+        var input = 9;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "Fizz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Given_GetResult_WithInput_15_ShouldReturn_String_FizzBuzz()
+    {
+        // Arrange
+        var input = 15;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "FizzBuzz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Given_GetResult_WithInput_20_ShouldReturn_String_Buzz()
+    {
+        // Arrange
+        var input = 20;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "Buzz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Given_GetResult_WithInput_30_ShouldReturn_String_FizzBuzz()
+    {
+        // Arrange
+        var input = 30;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "FizzBuzz";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Given_GetResult_WithInput_75_Should_Return_String_FizzBuzz()
+    {
+        // Arrange
+        var input = 75;
+        var fizzBuzz = new FizzBuzz();
+        // Act
+        var result = fizzBuzz.GetResult(input);
+        // Assert
+        var expected = "FizzBuzz";
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
--- FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -4,6 +4,31 @@ public class FizzBuzz
 {
     public string GetResult(int input)
     {
-        throw new NotImplementedException();
+        if (IsDivisibleByThree(input) && IsDivisibleByFive(input))
+        {
+            return "FizzBuzz";
+        }
+
+        if (IsDivisibleByThree(input))
+        {
+            return "Fizz";
+        }
+
+        if (IsDivisibleByFive(input))
+        {
+            return "Buzz";
+        }
+
+        return input.ToString();
+    }
+
+    private bool IsDivisibleByThree(int input)
+    {
+        return input % 3 == 0;
+    }
+
+    private bool IsDivisibleByFive(int input)
+    {
+        return input % 5 == 0;
     }
 }
